4639279e7a8c2f203691ce91b52df2b7dfbb0af6
moowoo <0109963@naver.com>
2024-06-08 11:17:32 +0900

    돔을 이용하여 html 객체에 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,24 @@
 "use strict";
 
-console.log("안녕");
-console.log("바이");
+// #은 태그의 id에 부여되어있는 것을 가져오는 것 -> <input id="이부분" />
+const mid = document.querySelector("#mid"),
+    pwd = document.querySelector("#pwd"),
+    loginBtn = document.querySelector("button");
+
+// console.log(mid);
+// console.log(pwd);
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    // console.log("로그인함수작동");
+    // console.log(mid.value);
+
+    const req = {
+        mid: mid.value,
+        pwd: pwd.value,
+    };
+
+    console.log(req);
+
+}
