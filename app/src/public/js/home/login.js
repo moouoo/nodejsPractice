9e917e3daa6bebb9c3555036ecbcff93167e043e
moowoo <0109963@naver.com>
2024-06-10 01:47:24 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const { post } = require("../../../routes/home");
-
-
-
 // #은 태그의 id에 부여되어있는 것을 가져오는 것 -> <input id="이부분" />
 const mid = document.querySelector("#mid"),
     pwd = document.querySelector("#pwd"),
@@ -32,5 +28,13 @@ function login(){
             "Content-Type" : "application/json"
         },
         body : JSON.stringify(req)
-    });
+    })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+
+    // res.json()의 반환값은 Promise다.
+    // 기본 res의 반환 값은 Responce스트림인데, .json()매서드를 통해 responce(응답)스트림을 읽을 수 있다.
+    // responce는 데이터가 모두 받아진 상태가 아니다.
+    // .json()으로 responce스트림을 가져와 완료될 때까지 읽는다. 다 읽은 body의 텍스트를 Promise 형태로 반환한다.
+
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,21 +1,62 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users = {
+    mid : ["admin", "신무우", "김팀장"],
+    pwd : ["1234", "1234", "123456"],
 };
 
-// 위에 있는거랑 똑같은 거임
-// function hello(req, res) {
+const output = {
+    home : (req, res) => {
+        res.render("home/index");
+    },
+
+    login : (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login : (req ,res) => {
+        // console.log(req.body);
+        const mid = req.body.mid,
+        pwd = req.body.pwd;
+
+        if(users.mid.includes(mid)){
+            const idx = users.mid.indexOf(mid);
+            if(users.pwd[idx] === pwd){
+                return res.json({
+                    success : true,
+                });
+            };
+        };
+
+        return res.json({
+            success : false,
+            msg : "로그인 실패",
+        });
+    },
+};
+
+// const home = (req, res) => {
 //     res.render("home/index");
 // };
 
-const login = (req, res) => {
-    res.render("home/login");
-};
+// 위에 있는거랑 똑같은 거임
+// function home(req, res) {
+//     res.render("home/index");
+// };
+
+// const login = (req, res) => {
+//     res.render("home/login");
+// };
 
 // 오브젝트로 빼준다
-module.exports = {
-    home,
-    login,
-};
+// module.exports = {
+//     home,
+//     login,
+// };
 
+module.exports = {
+    output,
+    process,
+};
